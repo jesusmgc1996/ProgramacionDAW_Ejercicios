b77f8bcd25de051ae5ad82e775e455ebb86442e2
jesusmgc1996 <jesusmgc1996@gmail.com>
2023-02-08 09:28:39 +0100

    fix (chapter 7 / block 1): Added sizes on exercise 1

--- src/capitulo07/bloque01/Ejercicio01_ValoresMaximosYMinimos.java
+++ src/capitulo07/bloque01/Ejercicio01_ValoresMaximosYMinimos.java
@@ -3,12 +3,18 @@ package capitulo07.bloque01;
 public class Ejercicio01_ValoresMaximosYMinimos {
 
 	public static void main(String[] args) {
-		System.out.println("Byte:\tLímite mínimo: " + Byte.MIN_VALUE + "\t\t\tLímite máximo: " + Byte.MAX_VALUE);
-		System.out.println("Int:\tLímite mínimo: " + Integer.MIN_VALUE + "\t\tLímite máximo: " + Integer.MAX_VALUE);
-		System.out.println("Short:\tLímite mínimo: " + Short.MIN_VALUE + "\t\t\tLímite máximo: " + Short.MAX_VALUE);
-		System.out.println("Long:\tLímite mínimo: " + Long.MIN_VALUE + "\tLímite máximo: " + Long.MAX_VALUE);
-		System.out.println("Float:\tLímite mínimo: " + Float.MIN_VALUE + "\t\t\tLímite máximo: " + Float.MAX_VALUE);
-		System.out.println("Double:\tLímite mínimo: " + Double.MIN_VALUE + "\t\t\tLímite máximo: " + Double.MAX_VALUE);
+		System.out.println("Byte:\t\tTamaño: " + Byte.SIZE + " bits\t\tLímite mínimo: "
+				+ Byte.MIN_VALUE + "\t\t\tLímite máximo: " + Byte.MAX_VALUE);
+		System.out.println("Int:\t\tTamaño: " + Integer.SIZE + " bits\t\tLímite mínimo: "
+				+ Integer.MIN_VALUE + "\t\tLímite máximo: " + Integer.MAX_VALUE);
+		System.out.println("Short:\t\tTamaño: " + Short.SIZE + " bits\t\tLímite mínimo: "
+				+ Short.MIN_VALUE + "\t\t\tLímite máximo: " + Short.MAX_VALUE);
+		System.out.println("Long:\t\tTamaño: " + Long.SIZE + " bits\t\tLímite mínimo: "
+				+ Long.MIN_VALUE + "\tLímite máximo: " + Long.MAX_VALUE);
+		System.out.println("Float:\t\tTamaño: " + Float.SIZE + " bits\t\tLímite mínimo: "
+				+ Float.MIN_VALUE + "\t\t\tLímite máximo: " + Float.MAX_VALUE);
+		System.out.println("Double:\t\tTamaño: " + Double.SIZE + " bits\t\tLímite mínimo: "
+				+ Double.MIN_VALUE + "\t\t\tLímite máximo: " + Double.MAX_VALUE);
 	}
 
 }
