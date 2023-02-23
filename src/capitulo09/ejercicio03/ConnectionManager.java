e16e9b894cf18a3310d4e12414d4fc375477f5ff
jesusmgc1996 <jesusmgc1996@gmail.com>
2023-02-23 14:21:31 +0100

    feat (chapter 9 / exercise 3): Added properties and connection files

---
+++ src/capitulo09/ejercicio03/ConnectionManager.java
@@ -0,0 +1,40 @@
+package capitulo09.ejercicio03;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+	private static Connection connection = null;
+	
+	public static Connection getConnection() throws SQLException {
+		// Si es la primera vez que se accede a la conexión, debe instanciarse
+		if (connection == null) {
+			connect();
+		}
+		// Se comprueba si la conexión sigue estando activa
+		while (!connection.isValid(5)) {
+			connect();
+		}
+		return connection;
+	}
+	
+	private static void connect() throws SQLException {
+		String driver = JDBCProperties.getProperty("JDBC_DRIVER_CLASS");
+		String user = JDBCProperties.getProperty("JDBC_USER");
+		String password = JDBCProperties.getProperty("JDBC_PASSWORD");
+		String host = JDBCProperties.getProperty("JDBC_HOST");
+		String schema = JDBCProperties.getProperty("JDBC_SCHEMA_NAME");
+		String properties = JDBCProperties.getProperty("JDBC_PROPERTIES");
+
+		try {
+			Class.forName(driver);
+			connection = (Connection) DriverManager.getConnection ("jdbc:mysql://" + host + "/" + schema + properties, user, password);			   
+		}
+		catch (ClassNotFoundException ex) {
+			System.out.println("Imposible acceder al driver Mysql");
+		}
+	}
+	
+}
